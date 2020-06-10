f908bfd8f99c94f7083c1f6ecdb5c181f43270ba
Branimir Radosavljevic <rbranimir@ymail.com>
2020-06-10 13:34:34 +0200

    Episode 10 & 11

--- app/Http/Livewire/RecentlyReviewed.php
+++ app/Http/Livewire/RecentlyReviewed.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class RecentlyReviewed extends Component
@@ -15,7 +16,7 @@ class RecentlyReviewed extends Component
         $before = Carbon::now()->subMonths(2)->timestamp;
         $current = Carbon::now()->timestamp;
         
-        $this->recentlyReviewed = Http::withHeaders(config('services.igdb'))
+        $recentlyReviewedUnformatted = Http::withHeaders(config('services.igdb'))
             ->withOptions([
                 'body' => "
                     fields name, cover.url, first_release_date, popularity, platforms.abbreviation, rating, rating_count, summary, slug;
@@ -29,11 +30,24 @@ class RecentlyReviewed extends Component
             ])->get('https://api-v3.igdb.com/games')
             ->json();
 
+        $this->recentlyReviewed = $this->formatForView($recentlyReviewedUnformatted);
+
     }
 
 
     public function render()
     {
         return view('livewire.recently-reviewed');
+    }
+
+    private function formatForView($games)
+    {
+        return collect($games)->map(function($game){
+            return collect($game)->merge([
+                'coverImageUrl' => Str::replaceFirst('thumb','cover_big', $game['cover']['url']),
+                'rating' => isset($game['rating']) ? round($game['rating']).'%' : null,
+                'platforms' => collect($game['platforms'])->pluck('abbreviation')->implode(', '),
+            ]);
+        })->toArray();
     }
 }
